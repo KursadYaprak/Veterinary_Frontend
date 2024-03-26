454efa3cd05d9da8ba8db33a4a1d757b1c498d93
Kürşad Yaprak <100860710+KursadYaprak@users.noreply.github.com>
2024-03-26 07:21:11 +0300

    Update AvailableDateService.js

--- src/services/AvailableDateService.js
+++ src/services/AvailableDateService.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api/v1/available_date';
+const BASE_URL = 'https://final-project-patika.onrender.com/api/v1/available_date';
 
 const AvailableDateService = {
   getAvailableDates: async () => {
